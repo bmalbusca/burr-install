30376461432c0cc15d927f36ce25209115693d9d
brunocfigueiredo <bmalbusca@gmail.com>
2020-02-09 21:58:35 +0000

    Export path .bashrc

--- burr-install.sh
+++ burr-install.sh
@@ -5,3 +5,4 @@ sudo apt-get install -y python3
 sudo apt-get install -y git
 sudo apt-get update
 sudo mkdir /usr/bin/burr; sudo chmod 777 /usr/bin/burr ; sudo cd  /usr/bin/burr ; sudo git -C /usr/bin/burr clone https://github.com/bmalbusca/burr-install.git
+python3  ./.install
