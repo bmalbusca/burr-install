44bd1da104ed4396b1569ae9908297a479f644a7
Bruno Figueiredo <brunocfigueiredo@tecnico.ulisboa.pt>
2020-02-10 19:26:20 +0000

    burr app resource updated

--- burr-install.sh
+++ burr-install.sh
@@ -4,5 +4,5 @@ sudo apt-get update
 sudo apt-get install -y python3
 sudo apt-get install -y git
 sudo apt-get update
-sudo mkdir /usr/bin/burr; sudo chmod 777 /usr/bin/burr ; sudo cd  /usr/bin/burr ; sudo git -C /usr/bin/burr clone https://github.com/bmalbusca/burr-install.git
+sudo mkdir /usr/bin/burr; sudo chmod 777 /usr/bin/burr ; sudo cd  /usr/bin/burr ; sudo git -C /usr/bin/ https://gitlab.com/awesome-dev/burr.git 
 python3  ./.install
